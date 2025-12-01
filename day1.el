e78509ae15ab90370ebffbe5487f2003006ad071
david feller <dave.feller@gmail.com>
2025-12-01 10:05:55 -0700

    use count clause

--- day1.el
+++ day1.el
@@ -5,6 +5,6 @@
            for i in (string-split (buffer-substring (point-min) (point-max)))
            for newdial = (+ dial (string-to-number i))
            sum (floor (abs newdial) 100) into part2
-           when (and (/= 0 dial) (<= newdial 0)) sum 1 into part2
-           when (= 0 (setf dial (mod newdial 100))) sum 1 into part1
+           count (and (/= 0 dial) (<= newdial 0)) into part2
+           count (= 0 (setf dial (mod newdial 100))) into part1
            finally return (list part1 part2)))
