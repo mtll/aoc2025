8cf887c158faa2dfd9384e5507dcbce45adedf62
david feller <dave.feller@gmail.com>
2025-12-01 15:57:18 -0700

    use kbd macro

--- day1.el
+++ day1.el
@@ -1,10 +1,14 @@
 ;;; -*- lexical-binding: t -*-
 
-(with-current-buffer "aoc1"
-  (cl-loop with dial = 50
+(save-window-excursion
+  (find-file "aoc1")
+  (execute-kbd-macro (kbd "M-< M-% r RET RET ! M-< M-% l RET - RET !"))
+  (cl-loop for dial = 50 then (mod newdial 100)
            for i in (string-split (buffer-substring (point-min) (point-max)))
            for newdial = (+ dial (string-to-number i))
            sum (floor (abs newdial) 100) into part2
            count (and (/= 0 dial) (<= newdial 0)) into part2
-           count (= 0 (setf dial (mod newdial 100))) into part1
-           finally return (list part1 part2)))
+           count (= 0 dial) into part1
+           finally return (progn (revert-buffer nil t)
+                                 (list part1 part2))))
+
