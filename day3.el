3196754204f6031cd8852f8603d0032ac879ed7b
david feller <dave.feller@gmail.com>
2025-12-03 05:25:13 -0700

    fix

--- day3.el
+++ day3.el
@@ -1,17 +1,20 @@
 ;;; -*- lexical-binding: t -*-
 
-(defun solve ()
-  (cl-labels ((line-max (line n)
-                (cl-loop for start = 0 then (+ (seq-position seq d) start 1)
-                         for end from (- (length line) n -1) upto (length line)
-                         for seq = (seq-subseq line start end)
-                         for d = (seq-max seq)
-                         sum (- d ?0)))))
-  (with-work-buffer
-    (insert-file-contents-literally "aoc3")
-    (cl-loop until (eobp)
-             for line = (buffer-substring-no-properties (point) (pos-eol))
-             sum (line-max line 2) into part1
-             sum (line-max line 12) into part2
-             do (forward-line)
-             finally return (list part1 part2))))
+(defun solve (file)
+  (cl-labels ((line-max (line n &optional (acc 0))
+                (if (= n 0) acc
+                  (let ((d (seq-max (substring line 0 (- (length line) n -1)))))
+                    (line-max (substring line (1+ (seq-position line d)))
+                              (1- n)
+                              (+ (- d ?0) (* 10 acc)))))))
+    (with-work-buffer
+      (insert-file-contents-literally file)
+      (cl-loop until (eobp)
+               for line = (buffer-substring-no-properties (point) (pos-eol))
+               sum (line-max line 2) into part1
+               sum (line-max line 12) into part2
+               do (forward-line)
+               finally return (list part1 part2)))))
+
+
+
