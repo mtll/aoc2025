f6caa36908ece0c4c8bac2afbffd0a5dca869735
david feller <dave.feller@gmail.com>
2025-12-03 05:27:20 -0700

    rename

--- day3.el
+++ day3.el
@@ -1,18 +1,18 @@
 ;;; -*- lexical-binding: t -*-
 
 (defun solve (file)
-  (cl-labels ((line-max (line n &optional (acc 0))
+  (cl-labels ((max-jolts (line n &optional (acc 0))
                 (if (= n 0) acc
                   (let ((d (seq-max (substring line 0 (- (length line) n -1)))))
-                    (line-max (substring line (1+ (seq-position line d)))
-                              (1- n)
-                              (+ (- d ?0) (* 10 acc)))))))
+                    (max-jolts (substring line (1+ (seq-position line d)))
+                               (1- n)
+                               (+ (- d ?0) (* 10 acc)))))))
     (with-work-buffer
       (insert-file-contents-literally file)
       (cl-loop until (eobp)
                for line = (buffer-substring-no-properties (point) (pos-eol))
-               sum (line-max line 2) into part1
-               sum (line-max line 12) into part2
+               sum (max-jolts line 2) into part1
+               sum (max-jolts line 12) into part2
                do (forward-line)
                finally return (list part1 part2)))))
 
